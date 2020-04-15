d10c21f47ea3893848cb137b9d83ea452c0ed115
marlene <rouzier.marlene@gmail.com>
2020-04-15 17:23:58 +0200

    réinitialisation des pages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,277 +1,84 @@
 <template>
-  <!-- FORMULAIRE -->
-  <div class="container-fluid body p-5">
-    <h1
-      class="phrase font-weight-bold text-center"
-    >Vous souhaitez prendre contact avec moi, merci de remplir ce formulaire. A bientôt !</h1>
+  <section class="section section-shaped section-lg my-0">
+    <div class="shape shape-style-1 bg-gradient-default">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <div class="container pt-lg-md">
+      <div class="row justify-content-center">
+        <div class="col-lg-5">
+          <card
+            type="secondary"
+            shadow
+            header-classes="bg-white pb-5"
+            body-classes="px-lg-5 py-lg-5"
+            class="border-0"
+          >
+            <template>
+              <div class="text-muted text-center mb-3">
+                <small>Sign in with</small>
+              </div>
+              <div class="btn-wrapper text-center">
+                <base-button type="neutral">
+                  <img slot="icon" src="img/icons/common/github.svg" />
+                  Github
+                </base-button>
 
-    <form id="myForm">
-      <span class="form_col">Sexe:</span>
-      <label>
-        <input name="sex" type="radio" value="H" />Homme
-      </label>
-      <label>
-        <input name="sex" type="radio" value="F" />Femme
-      </label>
-      <span class="erreur">Vous devez sélectionnez votre sexe</span>
-      <br />
-      <br />
-
-      <label class="form_col" for="lastName">Nom:</label>
-      <input name="lastName" id="lastName" type="text" />
-      <span class="erreur">Un nom ne peut pas faire moins de 2 caractères</span>
-      <br />
-      <br />
-
-      <label class="form_col" for="firstName">Prénom:</label>
-      <input name="firstName" id="firstName" type="text" />
-      <span class="erreur">Un prénom ne peut pas faire moins de 2 caractères</span>
-      <br />
-      <br />
-
-      <label class="form_col" for="age">Âge:</label>
-      <input name="age" id="age" type="text" />
-      <span class="erreur">L'âge doit être compris entre 5 et 140</span>
-      <br />
-      <br />
-
-      <label class="form_col" for="login">Pseudo:</label>
-      <input name="login" id="login" type="text" />
-      <span class="erreur">Le pseudo ne peut pas faire moins de 4 caractères</span>
-      <br />
-      <br />
-
-      <label class="form_col" for="login">Email:</label>
-      <input name="email" id="email" type="text" />
-      <span
-        class="erreur"
-      >L'adresse mail doit contenir un nom d'utilisateur, le caractère @ et un nom de domaine</span>
-      <br />
-      <br />
-
-      <label class="form_col" for="pwd1">Mot de passe:</label>
-      <input name="pwd1" id="pwd1" type="password" />
-      <span class="erreur">Le mot de passe ne doit pas faire moins de 6 caractères</span>
-      <br />
-      <br />
-
-      <label class="form_col MDP" for="pwd2">Mot de passe (confirmation):</label>
-      <label class="form_col d-lg-none" for="pwd2">Confirmer MDP:</label>
-      <input name="pwd2" id="pwd2" type="password" />
-      <span class="erreur">Le mot de passe de confirmation doit être identique à celui d'origine</span>
-      <br />
-      <br />
-
-      <label class="form_col" for="country">Pays :</label>
-      <select name="country" id="country">
-        <option value="none">Sélectionnez votre pays de résidence</option>
-        <option value="en">Angleterre</option>
-        <option value="us">États-Unis</option>
-        <option value="fr">France</option>
-      </select>
-      <span class="erreur">Vous devez sélectionner votre pays de résidence</span>
-      <br />
-      <br />
-
-      <span class="form_col"></span>
-      <label>
-        <input name="news" type="checkbox" /> Je désire recevoir la newsletter chaque mois.
-      </label>
-      <br />
-      <br />
-
-      <span class="form_col"></span>
-      <input type="submit" value="M'inscrire" />
-      <input type="reset" value="Réinitialiser le formulaire" />
-    </form>
-  </div>
+                <base-button type="neutral">
+                  <img slot="icon" src="img/icons/common/google.svg" />
+                  Google
+                </base-button>
+              </div>
+            </template>
+            <template>
+              <div class="text-center text-muted mb-4">
+                <small>Or sign in with credentials</small>
+              </div>
+              <form role="form">
+                <base-input
+                  alternative
+                  class="mb-3"
+                  placeholder="Email"
+                  addon-left-icon="ni ni-email-83"
+                ></base-input>
+                <base-input
+                  alternative
+                  type="password"
+                  placeholder="Password"
+                  addon-left-icon="ni ni-lock-circle-open"
+                ></base-input>
+                <base-checkbox>Remember me</base-checkbox>
+                <div class="text-center">
+                  <base-button type="primary" class="my-4">Sign In</base-button>
+                </div>
+              </form>
+            </template>
+          </card>
+          <div class="row mt-3">
+            <div class="col-6">
+              <a href="#" class="text-light">
+                <small>Forgot password?</small>
+              </a>
+            </div>
+            <div class="col-6 text-right">
+              <a href="#" class="text-light">
+                <small>Create new account</small>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
 export default {};
-
-// Fonction de désactivation de l'affichage des "tooltips"
-function deactivateTooltips() {
-  var tooltips = document.querySelectorAll(".erreur"),
-    tooltipsLength = tooltips.length;
-
-  for (var i = 0; i < tooltipsLength; i++) {
-    tooltips[i].style.display = "none";
-  }
-}
-
-// La fonction ci-dessous permet de récupérer la "tooltip" qui correspond à notre input
-
-function getTooltip(elements) {
-  while ((elements = elements.nextSibling)) {
-    if (elements.className === "erreur") {
-      return elements;
-    }
-  }
-  return false;
-}
-
-// Fonctions de vérification du formulaire, elles renvoient "true" si tout est ok
-var check = {}; // On met toutes nos fonctions dans un objet littéral
-
-check["sex"] = function() {
-  var sex = document.getElementsByName("sex"),
-    tooltipStyle = getTooltip(sex[1].parentNode).style;
-
-  if (sex[0].checked || sex[1].checked) {
-    tooltipStyle.display = "none";
-    return true;
-  } else {
-    tooltipStyle.display = "inline-block";
-    return false;
-  }
-};
-
-check["lastName"] = function(id) {
-  var name = document.getElementById(id),
-    tooltipStyle = getTooltip(name).style;
-
-  if (name.value.length >= 2) {
-    name.className = "correct";
-    tooltipStyle.display = "none";
-    return true;
-  } else {
-    name.className = "incorrect";
-    tooltipStyle.display = "inline-block";
-    return false;
-  }
-};
-
-check["firstName"] = check["lastName"]; // La fonction pour le prénom est la même que celle du nom
-
-check["age"] = function() {
-  var age = document.getElementById("age"),
-    tooltipStyle = getTooltip(age).style,
-    ageValue = parseInt(age.value);
-
-  if (!isNaN(ageValue) && ageValue >= 5 && ageValue <= 140) {
-    age.className = "correct";
-    tooltipStyle.display = "none";
-    return true;
-  } else {
-    age.className = "incorrect";
-    tooltipStyle.display = "inline-block";
-    return false;
-  }
-};
-
-check["login"] = function() {
-  var login = document.getElementById("login"),
-    tooltipStyle = getTooltip(login).style;
-
-  if (login.value.length >= 4) {
-    login.className = "correct";
-    tooltipStyle.display = "none";
-    return true;
-  } else {
-    login.className = "incorrect";
-    tooltipStyle.display = "inline-block";
-    return false;
-  }
-};
-
-check["email"] = function() {
-  var email = document.getElementById("email"),
-    tooltipStyle = getTooltip(email).style;
-
-  var pattern = /^[a-z0-9\-_\.]+@[a-z0-9]+\.[a-z]{2,5}$/;
-  if (email.value.match(pattern)) {
-    email.className = "correct";
-    tooltipStyle.display = "none";
-    return true;
-  } else {
-    email.className = "incorrect";
-    tooltipStyle.display = "inline-block";
-    return false;
-  }
-};
-
-check["pwd1"] = function() {
-  var pwd1 = document.getElementById("pwd1"),
-    tooltipStyle = getTooltip(pwd1).style;
-
-  if (pwd1.value.length >= 6) {
-    pwd1.className = "correct";
-    tooltipStyle.display = "none";
-    return true;
-  } else {
-    pwd1.className = "incorrect";
-    tooltipStyle.display = "inline-block";
-    return false;
-  }
-};
-
-check["pwd2"] = function() {
-  var pwd1 = document.getElementById("pwd1"),
-    pwd2 = document.getElementById("pwd2"),
-    tooltipStyle = getTooltip(pwd2).style;
-
-  if (pwd1.value == pwd2.value && pwd2.value != "") {
-    pwd2.className = "correct";
-    tooltipStyle.display = "none";
-    return true;
-  } else {
-    pwd2.className = "incorrect";
-    tooltipStyle.display = "inline-block";
-    return false;
-  }
-};
-
-check["country"] = function() {
-  var country = document.getElementById("country"),
-    tooltipStyle = getTooltip(country).style;
-
-  if (country.options[country.selectedIndex].value != "none") {
-    tooltipStyle.display = "none";
-    return true;
-  } else {
-    tooltipStyle.display = "inline-block";
-    return false;
-  }
-};
-
-// Mise en place des événements
-(function() {
-  // Utilisation d'une IIFE pour éviter les variables globales.
-
-  var myForm = document.getElementById("myForm"),
-    inputs = document.querySelectorAll(
-      "input[type=text], input[type=password]"
-    ),
-    inputsLength = inputs.length;
-
-  for (var i = 0; i < inputsLength; i++) {
-    inputs[i].addEventListener("keyup", function(e) {
-      check[e.target.id](e.target.id); // "e.target" représente l'input actuellement modifié
-    });
-  }
-
-  myForm.addEventListener("submit", function(e) {
-    var result = true;
-
-    for (var i in check) {
-      result = check[i](i) && result;
-    }
-    if (result) {
-      alert("Le formulaire est bien rempli.");
-    }
-    e.preventDefault();
-  });
-
-  myForm.addEventListener("reset", function() {
-    for (var i = 0; i < inputsLength; i++) {
-      inputs[i].className = "";
-    }
-    deactivateTooltips();
-  });
-})();
-
-// Maintenant que tout est initialisé, on peut désactiver les "tooltips"
-deactivateTooltips();
 </script>
 <style>
 </style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,39 +1,78 @@
 <template>
   <div class="profile-page">
-    <div class="container-fluid body">
-      <div class="text">
-        <h1 class="phrase font-weight-bold text-center">Préparer l'arrivée de mon lapin</h1>
-
-        <p
-          class="espace"
-        >Lors de son arrivée à la maison, il est essentiel de permettre à votre lapin de s’habituer en douceur à son nouvel environnement. Vous aurez prévu à l’avance tous les éléments indispensables à son confort: gamelles, toilettes, jouets, coin-cachette pour dormir... Mais dans tous les cas, douceur, calme et patience sont de rigueur.</p>
-
-        <p
-          class="espace"
-        >Avant toute chose, il faut comprendre que dans la nature, le lapin est une proie. Il vit de manière discrète, s’enfuit à la moindre menace et passe du temps caché dans son terrier.Il ne faut donc pas s’étonner qu’il soit effrayé lorsqu’il est déposé sur le sol carrelé d’une pièce “vide”, entouré d’enfants joyeux! Si, dans ce cas, on tente de le saisir, il est possible qu’il cherche à s’échapper et griffe les mains qui le retiennent... Pas d’inquiétude: votre petit sauvageon doit apprendre à vous connaître. Pour cela, il est essentiel de le laisser se cacher pour se rassurer.</p>
-
-        <p class="espace">
-          Laissez-lui du temps. Respectez sa timidité. Tant qu’il reste caché, il doit pouvoir avoir accès à l’eau, à sa nourriture et à ses toilettes. Au fils des jours, il apprendra à vous connaître, à reconnaître les buits de la maison.
-          C’est là qu’il va progressivement commencer à découvrir son territoire. Comme il le ferait dans la nature, il va se promener, de plus en plus longuement, pour repérer les cachettes où se réfugier, les zones où se trouvent des aliments, les endroits amusants... Laissez-le évoluer librement (en sécurité), et permettez-lui de découvrir la maison et la famille, sans tenter de l’attraper.
-        </p>
-
-        <p
-          class="espace"
-        >Lors des premiers contacts, soyez très calme. Laissez-le s’apporcher de vous sans bouger. S’il vient vous renifler, vous pouvez lui parler doucement, mais dans tous les cas, il doit pouvoir s’enfuir comme bon lui semble, car aucun lien de confiance ne peut se construire sous la contrainte.</p>
-
-        <p
-          class="espace"
-        >Progressivement, il va faire connaissance avec son nouvel environnement et gagner en confiance. Certains membres de la famille sauront plus facilement l’apprivoiser, mais tout est affaire de patience et de douceur. Au fil des semaines, il reconnaîtra les voix, appréciera les caresses, et découvrira la hiérarchie du foyer.</p>
-
-        <div class="text-center">
-          <img
-            src="../../public/img/images/cage-pour-lapin.jpg"
-            alt="cage pour lapin"
-            class="img-fluid"
-          />
-        </div>
+    <section class="section-profile-cover section-shaped my-0">
+      <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
-    </div>
+    </section>
+    <section class="section section-skew">
+      <div class="container">
+        <card shadow class="card-profile mt--300" no-body>
+          <div class="px-4">
+            <div class="row justify-content-center">
+              <div class="col-lg-3 order-lg-2">
+                <div class="card-profile-image">
+                  <a href="#">
+                    <img v-lazy="'img/theme/team-4-800x800.jpg'" class="rounded-circle" />
+                  </a>
+                </div>
+              </div>
+              <div class="col-lg-4 order-lg-3 text-lg-right align-self-lg-center">
+                <div class="card-profile-actions py-4 mt-lg-0">
+                  <base-button type="info" size="sm" class="mr-4">Connect</base-button>
+                  <base-button type="default" size="sm" class="float-right">Message</base-button>
+                </div>
+              </div>
+              <div class="col-lg-4 order-lg-1">
+                <div class="card-profile-stats d-flex justify-content-center">
+                  <div>
+                    <span class="heading">22</span>
+                    <span class="description">Friends</span>
+                  </div>
+                  <div>
+                    <span class="heading">10</span>
+                    <span class="description">Photos</span>
+                  </div>
+                  <div>
+                    <span class="heading">89</span>
+                    <span class="description">Comments</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="text-center mt-5">
+              <h3>
+                Jessica Jones
+                <span class="font-weight-light">, 27</span>
+              </h3>
+              <div class="h6 font-weight-300">
+                <i class="ni location_pin mr-2"></i>Bucharest, Romania
+              </div>
+              <div class="h6 mt-4">
+                <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
+              </div>
+              <div>
+                <i class="ni education_hat mr-2"></i>University of Computer Science
+              </div>
+            </div>
+            <div class="mt-5 py-5 border-top text-center">
+              <div class="row justify-content-center">
+                <div class="col-lg-9">
+                  <p>An artist of considerable range, Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. An artist of considerable range.</p>
+                  <a href="#">Show more</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </card>
+      </div>
+    </section>
   </div>
 </template>
 <script>
